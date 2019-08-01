6479862f383e01be0f50e188f07c746b06816239
Serhii Zabolennyi <45561319+zabolennyi@users.noreply.github.com>
2019-08-01 15:54:19 +0200

    added check if today is holiday

--- cypress/integration/examples/fa.js
+++ cypress/integration/examples/fa.js
@@ -1,13 +1,10 @@
 //Cypress API Testing Factorial
 
-//set email as CI env variable
-//set  password as CI env variable
-//set employee_id as CI env variable 
+//set email/password/employee_id as CI env variables
 
 const currentDay = Cypress.moment().format('DD')
 const currentMonth = Cypress.moment().format('M')
 const currentYear = Cypress.moment().format('YYYY')
-
 const appOrigin = 'https://app.factorialhr.com'
 const apiOrigin = 'https://api.factorialhr.com'
 
@@ -18,13 +15,16 @@ const shiftToDelete = ''
 const email = Cypress.env('email')
 const password = Cypress.env('password')
 const employee_id = Cypress.env('employee_id')
+const calendarId = employee_id
 
+//generate random number to set as minutes 
 function randomNumber(min, max) {  
   return Math.floor(Math.random() * (max - min) + min); 
 }
 
-let randomMin = randomNumber(10, 59)
+const randomMin = randomNumber(10, 59)
 
+//login with credencials to obtain cookie
 describe('login', () => {
   Cypress.Commands.add("login", () => {
     return cy.request({
@@ -35,8 +35,8 @@ describe('login', () => {
         'Referer' : 'https://factorialhr.com/users/sign_in'
       },
       body: {
-        'user[email]':email,
-        'user[password]':password,
+        'user[email]': email,
+        'user[password]': password,
         'user[remember_me]': 0
       }
     }).then((response) => {
@@ -47,7 +47,7 @@ describe('login', () => {
     })
 })
 
-
+// get periods of current month
 describe('Get Periods', () => {
   Cypress.Commands.add("getPeriods", () =>  {
     cy.login().then(cookie => {
@@ -71,6 +71,7 @@ describe('Get Periods', () => {
   })
 })
 
+// get current day info and todays shift id
 describe('Get days in current month', () => {
   Cypress.Commands.add("getDaysInfoInCurrentMonth", () => {
     cy.getPeriods().then(current_month_id => {
@@ -86,7 +87,7 @@ describe('Get days in current month', () => {
         const date = new Date();
         const today = date.getDate();
         const currentDayInfo = daysInfoInCurrentMonth.find(x => x.day === today);
-        const todayShiftId = currentDayInfo.id
+        const todayShiftId = currentDayInfo.id 
         expect(response.status).to.eq(200)
         expect(daysInfoInCurrentMonth).to.exist
         expect(response.body[0].id).to.exist
@@ -97,87 +98,110 @@ describe('Get days in current month', () => {
   })
 })
 
-describe('set observation for today', () => {
-  it('should successfully set observation for current day', () => {
-    cy.getDaysInfoInCurrentMonth().then(todayShiftId => {
-      cy.getPeriods().then(current_month_id => {
-        cy.request({
-          method: 'patch',
-          url: apiOrigin + '/attendance/shifts/' + todayShiftId,
-          headers: {
-            'Referer': appOrigin + '/attendance/clock-in/' + currentYear + '/' + currentMonth,
-            'Origin':appOrigin,
-            'Content-type':'application/json' 
-          },
-          body: {
-            "observations": observation
-          }
-        })
-        .then((response) => {
-          expect(response.status).to.eq(200)
-          expect(response.body).to.not.be.null
-          expect(response.body.observations).to.eq(observation)
-          console.log(response)
-        })
+//get calendar and days details
+describe('Get Calendar', () => {
+  Cypress.Commands.add("getTodayLeaveStatus", () =>  {
+    cy.login().then(cookie => {
+       cy.request({
+        method: 'get',
+        url: apiOrigin + '/attendance/calendar',
+        qs: {
+          id: calendarId,
+          year: currentYear,
+          month: currentMonth
+        }
+      })
+      .then((response) => {
+        const currentLeaveDayInfo = response.body
+        expect(response.status).to.eq(200)
+        expect(currentLeaveDayInfo).to.not.be.null
+        return currentLeaveDayInfo
       })
     })
   })
 })
 
-describe('check-in', () => {
-  it('should successfully check-in', () => {
-    cy.getPeriods().then(current_month_id => {
-    //set morning time
-    cy.request({
-      method: 'post',
-      url: apiOrigin + '/attendance/shifts',
-      headers: {
-        'Referer': appOrigin + '/attendance/shifts/clock-in/' + currentYear + '/' + currentMonth,
-        'Origin':appOrigin,
-        'Content-type':'application/json' 
-      },
-      body: {
-        "period_id":current_month_id,
-        "minutes":0,
-        "day":currentDay,
-        "observations":null,
-        "clock_in":"08:" + randomMin,
-        "clock_out":"13:00"
-      }
-    })
-    .then((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body).to.exist
-      expect(response.body).to.not.be.null
-      expect(response.body.id).to.exist
+//check-in for today
+describe('check-in and set observation for today workday', () => {
+  it('should successfully set observation for current day', () => {
+    cy.getTodayLeaveStatus().then(currentLeaveDayInfo => {
+      //check if user has holiday/day-off today
+      if(currentLeaveDayInfo.is_leave === true || currentLeaveDayInfo.is_working === false) {
+        console.log("Today is Off-day")
+      } else {
+      cy.getPeriods().then(current_month_id => {
+        cy.request({
+          method: 'post',
+          url: apiOrigin + '/attendance/shifts',
+          headers: {
+            'Referer': appOrigin + '/attendance/shifts/clock-in/' + currentYear + '/' + currentMonth,
+            'Origin':appOrigin,
+            'Content-type':'application/json' 
+          },
+          body: {
+            period_id: current_month_id,
+            minutes: 0,
+            day: currentDay,
+            observations: null,
+            clock_in: "08:" + randomMin,
+            clock_out: "13:00"
+          }
+        })
+        .then((response) => {
+          expect(response.status).to.eq(201)
+          expect(response.body).to.exist
+          expect(response.body).to.not.be.null
+          expect(response.body.id).to.exist
+          })
+          cy.request({
+            method: 'post',
+            url: apiOrigin + '/attendance/shifts',
+            headers: {
+              'Referer': appOrigin + '/attendance/shifts/clock-in/' + currentYear + '/' + currentMonth,
+              'Origin':appOrigin,
+              'Content-type':'application/json' 
+            },
+            body: {
+              period_id: current_month_id,
+              minutes: 0,
+              day: currentDay,
+              observations: null,
+              clock_in: "14:00",
+              clock_out: "17:" + randomMin
+            }
+          })
+          .then((response) => {
+            expect(response.status).to.eq(201)
+            expect(response.body).to.exist
+            expect(response.body).to.not.be.null
+            expect(response.body.id).to.exist
+         })
       })
-      //set evening time
-      cy.request({
-        method: 'post',
-        url: apiOrigin + '/attendance/shifts',
-        headers: {
-          'Referer': appOrigin + '/attendance/shifts/clock-in/' + currentYear + '/' + currentMonth,
-          'Origin':appOrigin,
-          'Content-type':'application/json' 
-        },
-        body: {
-          "period_id":current_month_id,
-          "minutes":0,
-          "day":currentDay,
-          "observations":null,
-          "clock_in":"14:00",
-          "clock_out":"17:" + randomMin
-        }
-      })
-      .then((response) => {
-        expect(response.status).to.eq(201)
-        expect(response.body).to.exist
-        expect(response.body).to.not.be.null
-        expect(response.body.id).to.exist
-     })
+      cy.getDaysInfoInCurrentMonth().then(todayShiftId => {
+        cy.getPeriods().then(current_month_id => {
+          cy.request({
+            method: 'patch',
+            url: apiOrigin + '/attendance/shifts/' + todayShiftId,
+            headers: {
+              'Referer': appOrigin + '/attendance/clock-in/' + currentYear + '/' + currentMonth,
+              'Origin':appOrigin,
+              'Content-type':'application/json' 
+            },
+            body: {
+              observations: observation
+            }
+          })
+          .then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.not.be.null
+            expect(response.body.observations).to.eq(observation)
+          })
+        })
+      })}
     })
   })
 })
+
 // 
 // describe('fill all shifts from selected one', () => {
 //   it('should successfully copy time from selected to all shifts', () => {
